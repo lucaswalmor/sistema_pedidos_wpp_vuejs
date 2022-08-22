30fa0f7790884926018a03fd326778f2dbfb73ea
lucaswalmor <69168254+lucaswalmor@users.noreply.github.com>
2022-08-22 13:31:09 -0300

    att

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -86,8 +86,8 @@ export default {
             };
             // transforma o array de dados do pedido em texto 
             const dataJson = JSON.stringify(data);
-            const req = await fetch("http://127.0.0.1:8000/api/login", {
-            // const req = await fetch("https://pedidoparrilha.herokuapp.com/api/login", {
+            // const req = await fetch("http://127.0.0.1:8000/api/login", {
+            const req = await fetch("https://pedidoparrilha.herokuapp.com/api/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: dataJson
